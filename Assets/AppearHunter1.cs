abe14dc102a229341e98495f253c7739f97d879e
駒井遥 <mariogalaxy3kibou@yahoo.co.jp>
2017-05-28 15:34:32 +0900

    ハンター

---
+++ Assets/AppearHunter1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearHunter1 : MonoBehaviour {
+	public GameObject Hunter;
+	public int m = 1;
+	// Use this for initialization
+	void Start () {
+		Hunter = GameObject.Find ("Hnunter1"); // 探して保存する		
+		Hunter.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Score.score > 5 & m == 1) {
+			m = 0;
+			Hunter.SetActive(true);
+		}
+	}
+}
